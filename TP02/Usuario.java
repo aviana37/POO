ba70759aea0326a28c3d7d7bb1badf16a91a7211
André Viana <a.nseven@live.com>
2013-05-02 11:46:24 -0300

    Revert "Revert "Base files at the moment""
    This reverts commit 4d65a940925f994615a8e4ad2c4b35a2979f0215.

---
+++ TP02/Usuario.java
@@ -0,0 +1,26 @@
+class Usuario
+{
+	private static int instances;
+	private int id;
+	private String nome, cpf, email;
+	private Data data;
+	private Endereco endereco;
+	
+	public Usuario(String Nome, String CPF, String Email, Data data, Endereco endereco)
+	{
+		nome = Nome; cpf = CPF; email = Email; this.data = data;
+		this.endereco = endereco;
+		id = ++instances;
+	}
+
+	public int getID() { return id; }
+
+	public String toString()
+	{
+		return nome + " - ID " + id + "\n" +
+		       email + " CPF: " + cpf + "\n" +
+		       "Cadastrado em " + data + "\n" +
+		       "Endereco:\n" + endereco;
+	}
+}
+
